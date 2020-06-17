156c1a86344e8d13f488050ada8bae8009cf7237
mdang <mdang@users.noreply.github.com>
2020-06-17 21:11:28 +0000

    deploy: ee16ba5f9fcbb71854f98e31361d6cbeb08bb571

---
+++ precache-manifest.386c379ae77f738ba54dd2c3ea142325.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5721f1a804fcfba8615e9d5d9a068ad1",
+    "url": "/b2b-for-good/index.html"
+  },
+  {
+    "revision": "a7d997620f2303a3c79b",
+    "url": "/b2b-for-good/static/css/main.5facb584.chunk.css"
+  },
+  {
+    "revision": "9b30cc21d6ccf78ffd87",
+    "url": "/b2b-for-good/static/js/2.e768379e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/b2b-for-good/static/js/2.e768379e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a7d997620f2303a3c79b",
+    "url": "/b2b-for-good/static/js/main.21801e9b.chunk.js"
+  },
+  {
+    "revision": "ca6cf02aa77d8991cfca",
+    "url": "/b2b-for-good/static/js/runtime-main.43c33fc5.js"
+  }
+]);
