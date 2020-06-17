2ea8f33b2444b71a6c85bd07e775f0cb945227fc
mdang <mdang@users.noreply.github.com>
2020-06-17 20:49:45 +0000

    deploy: 80306ead366092d8350eb113b5d8d4eebd892f39

---
+++ precache-manifest.4f16712db211ecbd83bac5a1fd708a0c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "da9a8056bfe72cb3f4f380da86ae6249",
+    "url": "/b2b-for-good/index.html"
+  },
+  {
+    "revision": "f3f168af0d0340e82bec",
+    "url": "/b2b-for-good/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "917459c42c56c9b5f1e4",
+    "url": "/b2b-for-good/static/js/2.703b449e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/b2b-for-good/static/js/2.703b449e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f3f168af0d0340e82bec",
+    "url": "/b2b-for-good/static/js/main.5be3d844.chunk.js"
+  },
+  {
+    "revision": "ca6cf02aa77d8991cfca",
+    "url": "/b2b-for-good/static/js/runtime-main.43c33fc5.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/b2b-for-good/static/media/logo.5d5d9eef.svg"
+  }
+]);
